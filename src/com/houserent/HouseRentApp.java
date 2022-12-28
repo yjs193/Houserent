189ff69d9cfc529ae5ece99e6c9c73cb2a51e650
yjs193 <johnson_193@qq.com>
2022-12-28 17:25:33 +0800

    Initial commit

---
+++ src/com/houserent/HouseRentApp.java
@@ -0,0 +1,10 @@
+package com.houserent;
+
+import com.houserent.view.HouseView;
+
+public class HouseRentApp {
+    public static void main(String[] args) {
+        new HouseView().mainMenu();
+        System.out.println("========您已退出房屋出租系统=======");
+    }
+}
